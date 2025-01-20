2afe30fda9f6227c45e80a7917a5130782d93292
ayyHA <ayyHaibaraAi@gmail.com>
2025-01-20 19:32:58 +0800

    is_copy_assignable,tuple upload

--- template_meta_programming/tmpv3.cpp
+++ template_meta_programming/tmpv3.cpp
@@ -1,5 +1,6 @@
 #include <cstdlib>
 #include <iostream>
+#include <tuple>
 #include <type_traits>
 #include <typeinfo>
 /*==================== EXAMPLE 1 enable_if ====================*/
@@ -137,6 +138,69 @@ using add_lvalue_reference_t = typename add_lvalue_reference<T>::type;
 template <typename T>
 using add_rvalue_reference_t = typename add_rvalue_reference<T>::type;
 
+/*==================== EXAMPLE 5 is_copy_assignable ====================*/
+/*
+  Through 'copy_assign_t' to get the data type,if opeartor= is allowed to
+  invoke.Via void_t to choose true_type or false type,which is similar with
+  'has_member_type'.
+*/
+template <typename T>
+using copy_assign_t = decltype(declval<T &>() = declval<const T &>());
+
+template <typename T, typename U = void>
+struct is_copy_assignable : false_type {};
+
+template <typename T>
+struct is_copy_assignable<T, void_t<copy_assign_t<T>>> : true_type {};
+
+template <typename T, typename U = void>
+constexpr bool is_copy_assignable_v = is_copy_assignable<T, U>::value;
+
+struct S5 {
+  S5 &operator=(const S5 &) = delete; // through '=delete' to abandon operator=
+};
+
+/*==================== EXAMPLE 6 self-definition tuple ====================*/
+/*
+  class template tuple written by recursive inherit, via initialize function to
+  achieve class template arguments deduction(CTAD),match partial specialization,
+  while argument is empty,match primary template.
+*/
+template <typename... Args> struct tuple {
+  tuple(Args...) {}
+};
+
+template <typename T, typename... Args>
+struct tuple<T, Args...> : tuple<Args...> {
+  T value_;
+  tuple(T v, Args... args) : value_(v), tuple<Args...>(args...) {}
+};
+
+/*
+  Through recursive methods to destruct tuple,like 'rank' to destruct array.
+*/
+template <unsigned int N, typename Tmpl> struct tuple_element;
+/* recursive inheritance */
+template <unsigned int N, typename T, typename... Args>
+struct tuple_element<N, tuple<T, Args...>>
+    : tuple_element<N - 1, tuple<Args...>> {};
+
+/* We can through tuple_element<>::type and tuple_element<>::__tuple_type to get
+ * some data type. */
+template <typename T, typename... Args>
+struct tuple_element<0, tuple<T, Args...>> : type_identity<T> {
+  using __tuple_type = tuple<T, Args...>;
+};
+
+template <unsigned int N, typename Tmpl>
+using tuple_element_t = typename tuple_element<N, Tmpl>::type;
+
+template <unsigned int N, typename... Args>
+tuple_element_t<N, tuple<Args...>> &get(tuple<Args...> &t) {
+  using tuple_type = typename tuple_element<N, tuple<Args...>>::__tuple_type;
+  return static_cast<tuple_type &>(t).value_;
+}
+
 int main() {
   /* =========== EXAMPLE 1 enable_if =========== */
   std::cout << "enable_if: " << std ::endl;
@@ -159,5 +223,19 @@ int main() {
   std::cout << is_same_v<int &, add_lvalue_reference_t<int>> << std::endl;
   std::cout << is_same_v<void, add_rvalue_reference_t<void>> << std::endl;
   std::cout << is_same_v<float &&, add_rvalue_reference_t<float>> << std::endl;
+
+  /* =========== EXAMPLE 5 is_copy_assignable =========== */
+  std::cout << "is_copy_assignable: " << std::endl;
+  std::cout << is_copy_assignable_v<int> << std::endl;
+  std::cout << is_copy_assignable_v<S5> << std::endl;
+
+  /* =========== EXAMPLE 6 tuple =========== */
+  std::cout << "tuple: " << std::endl;
+  // tuple<char,float,float,char> t = tuple('1', 2.2f, 3.3f, 'a');
+  auto t = tuple('1', 2.2f, 3.3f, 'a', std::string("😄"));
+  std::cout << std::is_same_v<tuple_element<1, decltype(t)>::__tuple_type,
+                              tuple<float, float, char>> << std::endl;
+  std::cout << is_same_v<char, tuple_element_t<3, decltype(t)>> << std::endl;
+  std::cout << get<4>(t) << std::endl;
   return 0;
 }
